740467a8ecc9fad544de03381e6e0dcd6a059709
El-Ouali Abderrahman <el-ouali@insa-20671>
2025-12-13 15:33:45 +0100

    add

---
+++ 1_exploration.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 1. Chargement
+print("Chargement des données...")
+X = pd.read_csv('alt_acsincome_ca_features_85.csv')
+y = pd.read_csv('alt_acsincome_ca_labels_85.csv')
+y.rename(columns={'PINCP': 'TARGET'}, inplace=True)
+df = pd.concat([X, y], axis=1)
+
+# 2. Analyse Numérique (Âge et Heures de travail)
+# On regarde si l'âge et les heures sont liés au revenu
+print("Génération de la matrice de corrélation...")
+plt.figure(figsize=(8, 6))
+sns.heatmap(df[['AGEP', 'WKHP', 'TARGET']].corr(), annot=True, cmap='coolwarm', fmt='.2f')
+plt.title("Corrélation : Âge, Heures, Revenu")
+plt.savefig('analyse_correlation_numerique.png')
+print("-> Image 'analyse_correlation_numerique.png' créée.")
+
+# 3. Analyse Catégorielle (Études, Mariage, Sexe)
+# On regarde le % de riches dans chaque catégorie
+cols_a_analyser = ['SCHL', 'MAR', 'SEX']
+titles = ['Niveau Scolaire', 'Statut Marital', 'Sexe']
+
+for col, title in zip(cols_a_analyser, titles):
+    plt.figure(figsize=(10, 6))
+    # On calcule la moyenne de la cible (0 ou 1), ce qui donne le % de riches
+    sns.barplot(x=col, y='TARGET', data=df, palette='viridis', errorbar=None)
+    plt.title(f"Probabilité d'avoir >50k$ selon : {title}")
+    plt.ylabel("Probabilité (>50k)")
+    plt.xlabel(f"Code {col}")
+    plt.savefig(f'analyse_{col}.png')
+    print(f"-> Image 'analyse_{col}.png' créée.")
+
+print("Terminé ! Regardez les images générées.")
