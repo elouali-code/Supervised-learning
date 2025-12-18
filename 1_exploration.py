186c6a859be52f8b17104855c8c5309926764aeb
Abderrahman EL OUALI <87524355+elouali-code@users.noreply.github.com>
2025-12-18 18:05:05 +0100

    Refine comments and titles in 1_exploration.py
    Updated comments and titles for clarity in analysis sections.

--- 1_exploration.py
+++ 1_exploration.py
@@ -9,7 +9,6 @@ y.rename(columns={'PINCP': 'TARGET'}, inplace=True)
 df = pd.concat([X, y], axis=1)
 
 # 2. Analyse Numérique (Âge et Heures de travail)
-# On regarde si l'âge et les heures sont liés au revenu
 print("Génération de la matrice de corrélation...")
 plt.figure(figsize=(8, 6))
 sns.heatmap(df[['AGEP', 'WKHP', 'TARGET']].corr(), annot=True, cmap='coolwarm', fmt='.2f')
@@ -17,13 +16,11 @@ plt.title("Corrélation : Âge, Heures, Revenu")
 plt.savefig('analyse_correlation_numerique.png')
 
 # 3. Analyse Catégorielle (Études, Mariage, Sexe)
-# On regarde le % de riches dans chaque catégorie
 cols_a_analyser = ['SCHL', 'MAR', 'SEX']
 titles = ['Niveau Scolaire', 'Statut Marital', 'Sexe']
 
 for col, title in zip(cols_a_analyser, titles):
     plt.figure(figsize=(10, 6))
-    # On calcule la moyenne de la cible (0 ou 1), ce qui donne le % de riches
     sns.barplot(x=col, y='TARGET', data=df, palette='viridis', errorbar=None)
     plt.title(f"Probabilité d'avoir >50k$ selon : {title}")
     plt.ylabel("Probabilité (>50k)")
